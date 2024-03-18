06495eeeec6d730d7854f12de194448089bc74f3
prikol769 <prikol769@gmail.com>
2024-03-17 21:10:58 -0500

    added Cart component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
   const user = null;
@@ -71,7 +72,7 @@ const Navbar = () => {
                   )}
 
                   <div className="ml-4 flow-root lg:ml-6">
-                    {/* TODO CARD */}
+                    <Cart />
                   </div>
                 </div>
               </div>
