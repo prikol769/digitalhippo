0614d9986f0db4ab59935d066fcc613b27c9687c
prikol769 <prikol769@gmail.com>
2024-03-20 20:48:50 -0500

    added signIn api

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -11,7 +11,7 @@ export const authRouter = router({
       const { email, password } = input;
       const payload = await getPayloadClient();
 
-      //check if user already exists
+      // check if user already exists
       const { docs: users } = await payload.find({
         collection: "users",
         where: {
@@ -50,5 +50,29 @@ export const authRouter = router({
       if (!isVerified) throw new TRPCError({ code: "UNAUTHORIZED" });
 
       return { success: true };
+    }),
+
+  signIn: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input, ctx }) => {
+      const { email, password } = input;
+      const { res } = ctx;
+
+      const payload = await getPayloadClient();
+
+      try {
+        await payload.login({
+          collection: "users",
+          data: {
+            email,
+            password,
+          },
+          res,
+        });
+
+        return { success: true };
+      } catch (error) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+      }
     }),
 });
