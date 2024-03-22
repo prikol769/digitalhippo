5f7071841dab331154c3790e7c33775a4bbcc2a0
prikol769 <prikol769@gmail.com>
2024-03-22 11:12:50 -0500

    added ProductReel section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from "../components/MaxWidthWrapper";
 import { Button, buttonVariants } from "../components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
+import ProductReel from "@/components/ProductReel";
 
 const perks = [
   {
@@ -45,6 +46,12 @@ export default function Home() {
             <Button variant="ghost">Our quality promise &rarr;</Button>
           </div>
         </div>
+
+        <ProductReel
+          query={{ sort: "desc", limit: 4 }}
+          title="Brand new"
+          href="/products"
+        />
       </MaxWidthWrapper>
 
       <section className="border-t border-gray-200 bg-gray-50">
